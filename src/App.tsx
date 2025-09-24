2a35dd5e167f57fe65882964263129896c32f2e0
yes505304@ipactory.com <yes505304@ipactory.com>
2025-09-24 13:39:25 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import {
   mdiLock,
   mdiEmailOutline,
   mdiCalendarAccountOutline,
+  mdiCellphone,
+  mdiCheckCircleOutline,
+  mdiChevronUp,
+  mdiChevronDown,
+  mdiCheckBold,
 } from "@mdi/js";
 import "./App.css";
-import type { commonType } from "./assets/types/common";
+import type { commonType, termType } from "./assets/types/common";
+
+function Header() {
+  return <div className="naverTitle">NAVER</div>;
+}
 
 function InputIcon(props: commonType) {
   return (
     <div
+      className="inputCommon"
       style={{
-        display: "flex",
-        padding: "10px 10px",
-        border: "1px solid #6b6b6e",
-        width: "460px",
+        padding: "10px",
+        borderTopRightRadius: props.direction === "top" ? "5px" : "0px",
+        borderTopLeftRadius: props.direction === "top" ? "5px" : "0px",
+        borderBottomLeftRadius: props.direction === "down" ? "5px" : "0px",
+        borderBottomRightRadius: props.direction === "down" ? "5px" : "0px",
       }}
     >
       <Icon path={props.path} size={1} />
@@ -31,41 +42,185 @@ function InputIcon(props: commonType) {
 function CommonInfo() {
   return (
     <>
-      <InputIcon path={mdiAccountOutline} placeHolder="아이디" />
+      <div
+        className="inputCommon"
+        style={{
+          padding: "10px",
+          borderTopRightRadius: "5px",
+          borderTopLeftRadius: "5px",
+        }}
+      >
+        <Icon path={mdiAccountOutline} size={1} />
+        <input
+          placeholder="아이디"
+          style={{ width: "314px", marginLeft: "4px" }}
+        />
+        <span style={{ fontSize: "14px", marginLeft: "5px" }}>@naver.com</span>
+      </div>
       <InputIcon path={mdiLock} placeHolder="비밀번호" />
-      <InputIcon path={mdiEmailOutline} placeHolder="[선택] 이메일주소" />
+      <InputIcon
+        path={mdiEmailOutline}
+        placeHolder="[선택] 이메일주소"
+        direction="down"
+      />
     </>
   );
 }
 
 function PersonalInfo() {
+  const [isMan, setIsMan] = useState(true);
+  const [isKorean, setIsKorean] = useState(true);
   return (
     <>
-      <InputIcon path={mdiAccountOutline} placeHolder="이름" />
+      <InputIcon path={mdiAccountOutline} placeHolder="이름" direction="top" />
       <InputIcon
         path={mdiCalendarAccountOutline}
         placeHolder="생년월일 8자리"
       />
-      <div style={{ display: "flex", border: "1px solid #6b6b6e" }}>
-        <button>남자</button>
-        <button>여자</button>
-        <button>선택안함</button>
+      <div
+        className="inputCommon"
+        style={{
+          padding: "5px 10px",
+          borderBottomRightRadius: "5px",
+          borderBottomLeftRadius: "5px",
+        }}
+      >
+        <div
+          className="buttonContainer"
+          style={{
+            marginRight: "20px",
+          }}
+        >
+          <div
+            className="selectBox"
+            style={{ borderColor: isMan ? "#03c75a" : "#6e6b6e" }}
+            onClick={() => setIsMan(true)}
+          >
+            남자
+          </div>
+          <div
+            className="selectBox"
+            style={{ borderColor: !isMan ? "#03c75a" : "#6e6b6e" }}
+            onClick={() => setIsMan(false)}
+          >
+            여자
+          </div>
+        </div>
+        <div className="buttonContainer">
+          <div
+            className="selectBox"
+            style={{ borderColor: isKorean ? "#03c75a" : "#6e6b6e" }}
+            onClick={() => setIsKorean(true)}
+          >
+            내국인
+          </div>
+          <div
+            className="selectBox"
+            style={{ borderColor: !isKorean ? "#03c75a" : "#6e6b6e" }}
+            onClick={() => setIsKorean(false)}
+          >
+            외국인
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
 function PhoneNumber() {
-  const [country, setCountry] = useState("대한민국 +82");
   return (
     <>
-      <select value={country} onChange={(e) => setCountry(e.target.value)}>
-        <option value="미국/캐나다 +1">미국/캐나다 +1</option>
-        <option value="영국 +44">영국 +44</option>
-        <option value="일본 +81">일본 +81</option>
-        <option value="대한민국 +82">대한민국 +82</option>
-      </select>
-      <input placeholder="휴대전화번호" />
+      <div
+        className="inputCommon"
+        style={{
+          padding: "5px 10px",
+          margin: "10px",
+        }}
+      >
+        <Icon path={mdiCellphone} size={1}></Icon>
+        <input
+          placeholder="휴대전화번호"
+          style={{ marginLeft: "10px", width: "100%" }}
+        />
+      </div>
+    </>
+  );
+}
+
+function Term(prop: termType) {
+  const [clickTerm, setClilckTerm] = useState(false);
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Icon
+        path={mdiCheckBold}
+        size={0.5}
+        style={{
+          cursor: "pointer",
+          color: clickTerm ? "#03c75a" : "#6e6b6e",
+        }}
+        onClick={() => setClilckTerm((prev) => !prev)}
+      />
+      <div style={{ marginLeft: "10px" }}>{prop.text}</div>
+    </div>
+  );
+}
+
+function TermsAll() {
+  const [arrowDown, setArrowDown] = useState(true);
+  const [clickAll, setClickAll] = useState(false);
+
+  return (
+    <>
+      <div
+        className="inputCommon"
+        style={{
+          alignItems: "center",
+          justifyContent: "space-between", // 양 끝 정렬
+          padding: "5px 10px",
+        }}
+      >
+        {/* 왼쪽 그룹 */}
+        <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Icon
+            path={mdiCheckCircleOutline}
+            size={1}
+            style={{
+              cursor: "pointer",
+              color: clickAll ? "#03c75a" : "#6e6b6e",
+            }}
+            onClick={() => setClickAll((prev) => !prev)}
+          />
+          <div>
+            <span style={{ color: "#03c75a" }}>[필수]</span> 인증 약관 전체 동의
+          </div>
+        </div>
+
+        {/* 오른쪽 아이콘 */}
+        <Icon
+          path={arrowDown ? mdiChevronDown : mdiChevronUp}
+          size={1}
+          style={{ cursor: "pointer" }}
+          onClick={() => setArrowDown((prev) => !prev)}
+        />
+      </div>
+      {!arrowDown && (
+        <div
+          style={{
+            width: "460px",
+            padding: "10px",
+            display: "grid",
+            gridTemplateColumns: "repeat(2, 230px)", // 한 줄에 2개
+            gap: "10px",
+            border: "1px solid #6e6b6e",
+          }}
+        >
+          <Term text={"개인정보 이용"} clickAll={clickAll} />
+          <Term text={"고유식별정보 처리"} clickAll={clickAll} />
+          <Term text={"통신사 이용약관"} clickAll={clickAll} />
+          <Term text={"인증사 이용약관"} clickAll={clickAll} />
+          <Term text={"네이버 개인정보수집"} clickAll={clickAll} />
+        </div>
+      )}
     </>
   );
 }
@@ -76,26 +231,22 @@ function App() {
       style={{
         display: "flex",
         height: "100vh",
-        justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
       }}
     >
+      <Header />
       <CommonInfo />
-      <ul>
-        <li
-          style={{
-            width: "460px",
-            color: "red",
-            fontSize: "14px",
-            fontWeight: "lighter",
-          }}
-        >
-          아이디: 필수정보입니다.
-        </li>
-      </ul>
+      <div className="errorText">
+        <div className="dotStyle">• 아이디: 필수 정보입니다.</div>
+        <div className="dotStyle">• 비밀번호: 필수 정보입니다.</div>
+      </div>
       <PersonalInfo />
       <PhoneNumber />
+      <div className="errorText">
+        <div className="dotStyle">• 휴대전화번호: 필수 정보입니다.</div>
+      </div>
+      <TermsAll />
     </div>
   );
 }
--- src/assets/types/common.ts
+++ src/assets/types/common.ts
@@ -1,4 +1,10 @@
 export interface commonType {
   path: string;
   placeHolder: string;
+  direction?: string;
+}
+
+export interface termType {
+  text: string;
+  clickAll?: boolean;
 }
